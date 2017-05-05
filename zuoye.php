3c15e27971d6e5b166ded76134cba7d495554af1
945346899 <945346899@qq.com>
2017-05-05 10:59:39 +0800

    2017.5.5

--- zuoye.php
+++ zuoye.php
@@ -35,13 +35,56 @@
 	//echo "<img  src='./images/".rand(1,9).".png '>";//让随机数字与图片建立连接
 
 	
-	 $x=54320;
-	 if ($x%2==0) {
-	 	echo "为偶数";
-	 } else {
-	 	echo "为奇数";
-	 }
-	 
-	 
+	// $x=54320;
+	// if ($x%2==0) {
+	 //	echo "为偶数";
+	// } else {
+	// 	echo "为奇数";
+	// }
+	
 
+	/*$x=76;
+	if($x>100){
+		echo""
+	}else if($x<80&&$x>60){
+		echo 
+	}else if ($x)*/
+	/*$a=10;
+	$b=20;
+	$c=30;
+	if ($a>$b&&$a>$c){
+		echo "a最大";
+	}else{
+	if ($b>$c){
+		echo "a中间";
+	}else
+		echo "a最小";
+	}*/
+
+	$x=50;
+	$y=60;
+	$z=40;
+	$max;
+	$min;
+	if ($x>$y ){
+		$max=$x ;
+		$min=$y;
+	}else{
+		$max=$y;
+		$min=$x;
+	}if ($max>$z){
+		if ($z>$min){
+		echo "$max>$z>$min ";
+		}else {echo  "$max>$min>$z";
+	}
+	}else{echo "$z>$max>$min" ;//比较任意三个不等数的大小，并按顺序写出来
+	}
+		
+	
+	
+
+
+	  
+
+	  
 ?>
